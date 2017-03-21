049483b9c5ae4b29f0e93576b4a48707d3bf71b6
Zhang Wei <weeizhang@outlook.com>
2017-03-21 18:45:32 +0800

    exercises 1

--- a1_q1_exercises.py
+++ a1_q1_exercises.py
@@ -14,7 +14,7 @@ import tensorflow as tf
 
 x = tf.random_uniform([])  # Empty array as shape creates a scalar.
 y = tf.random_uniform([])
-out = tf.cond(tf.less(x, y), lambda: tf.add(x, y), lambda: tf.sub(x, y))
+out_1a = tf.cond(tf.less(x, y), lambda: tf.add(x, y), lambda: tf.subtract(x, y))
 
 ###############################################################################
 # 1b: Create two 0-d tensors x and y randomly selected from -1 and 1.
@@ -22,7 +22,10 @@ out = tf.cond(tf.less(x, y), lambda: tf.add(x, y), lambda: tf.sub(x, y))
 # Hint: Look up tf.case().
 ###############################################################################
 
-# YOUR CODE
+x = tf.random_uniform([], -1, 1)
+y = tf.random_uniform([], -1, 1)
+out_1b = tf.case({tf.less(x, y): lambda: tf.add(x, y), tf.greater(x, y): lambda: tf.subtract(x, y)},
+                 default=lambda: tf.constant(0.0), exclusive=True)
 
 ###############################################################################
 # 1c: Create the tensor x of the value [[0, -2, -1], [0, 1, 2]]
@@ -31,7 +34,9 @@ out = tf.cond(tf.less(x, y), lambda: tf.add(x, y), lambda: tf.sub(x, y))
 # Hint: Look up tf.equal().
 ###############################################################################
 
-# YOUR CODE
+x = tf.constant([[0, -2, -1], [0, 1, 2]])
+y = tf.zeros_like(x)
+out_1c = tf.equal(x, y)
 
 ###############################################################################
 # 1d: Create the tensor x of value
@@ -46,7 +51,12 @@ out = tf.cond(tf.less(x, y), lambda: tf.add(x, y), lambda: tf.sub(x, y))
 # Hint: Use tf.gather().
 ###############################################################################
 
-# YOUR CODE
+x = tf.constant([29.05088806, 27.61298943, 31.19073486, 29.35532951,
+                 30.97266006, 26.67541885, 38.08450317, 20.74983215,
+                 34.94445419, 34.45999146, 29.06485367, 36.01657104,
+                 27.88236427, 20.56035233, 30.20379066, 29.51215172,
+                 33.71149445, 28.59134293, 36.05556488, 28.66994858])
+out_1d = tf.gather(x, tf.where(tf.greater(x, 30)))
 
 ###############################################################################
 # 1e: Create a diagnoal 2-d tensor of size 6 x 6 with the diagonal values of 1,
@@ -54,7 +64,7 @@ out = tf.cond(tf.less(x, y), lambda: tf.add(x, y), lambda: tf.sub(x, y))
 # Hint: Use tf.range() and tf.diag().
 ###############################################################################
 
-# YOUR CODE
+out_1e = tf.diag(tf.range(1, 7, 1))
 
 ###############################################################################
 # 1f: Create a random 2-d tensor of size 10 x 10 from any distribution.
@@ -62,7 +72,7 @@ out = tf.cond(tf.less(x, y), lambda: tf.add(x, y), lambda: tf.sub(x, y))
 # Hint: Look at tf.matrix_determinant().
 ###############################################################################
 
-# YOUR CODE
+out_1f = tf.diag(tf.random_normal([10, 10], mean=10, stddev=1))
 
 ###############################################################################
 # 1g: Create tensor x with value [5, 2, 3, 5, 10, 6, 2, 3, 4, 2, 1, 1, 0, 9].
@@ -70,7 +80,8 @@ out = tf.cond(tf.less(x, y), lambda: tf.add(x, y), lambda: tf.sub(x, y))
 # Hint: use tf.unique(). Keep in mind that tf.unique() returns a tuple.
 ###############################################################################
 
-# YOUR CODE
+x = tf.constant([5, 2, 3, 5, 10, 6, 2, 3, 4, 2, 1, 1, 0, 9])
+unique_values, indices = tf.unique(x)
 
 ###############################################################################
 # 1h: Create two tensors x and y of shape 300 from any normal distribution,
@@ -82,4 +93,10 @@ out = tf.cond(tf.less(x, y), lambda: tf.add(x, y), lambda: tf.sub(x, y))
 # Hint: see the Huber loss function in the lecture slides 3.
 ###############################################################################
 
-# YOUR CODE
+x = tf.random_normal([300], mean=5, stddev=1)
+y = tf.random_normal([300], mean=5, stddev=1)
+average = tf.reduce_mean(x - y)
+condition = tf.less(average, 0)
+left_op = tf.reduce_mean(tf.square(x - y))
+right_op = tf.reduce_sum(tf.abs(x - y))
+out = tf.where(condition, left_op, right_op)
